e97bd0c9e234da6408158599dae014cfa27397e5
Nikita Sementcov <sementcov.nikita@gmail.com>
2021-01-10 21:22:33 +0300

    add one error handler, loader and a few event handlers

--- index.js
+++ index.js
@@ -1,57 +1,101 @@
+'use strict';
+
 const API_URL = 'https://api.exchangeratesapi.io';
-const LATEST_API_URL = `${API_URL}/latest`;
+const API_PATH = `${API_URL}/latest`;
 const PRECISION = 4;
-const API_CALL_INTERVAL = 60 * 60 * 1000;
 const LOCALE = 'en-US';
+const API_CALL_INTERVAL_MS = 60 * 60 * 1000;
 
 let rates = {};
-const selectors = document.getElementsByClassName('currency-selector');
+const selectors = Array.from(document.getElementsByClassName('currency-selector'));
+const loader = document.getElementById('loader');
 
-const fn = async () => {
-    const response = await fetch(LATEST_API_URL);
-    const data = await response.json();
-    const baseCurrency = data.base;
-    const {rates: baseCurrencyRates} = data;
-    rates = {[baseCurrency]: {...baseCurrencyRates, [baseCurrency]: 1}};
-    for (const currency of Object.keys(baseCurrencyRates)){
+const fetchRates = async () => {
+    try {
+        const response = await fetch(API_PATH);
+        return await response.json();
+    } catch (e) {
+        console.error(e);
+        loader.innerText = 'Can not get the rates data. Check your internet connection.';
+    }
+}
+
+const calculateRates = baseCurrencyRates => {
+    for (const currency of Object.keys(baseCurrencyRates)) {
         rates[currency] = {};
         const toBaseCurrencyRate = 1 / baseCurrencyRates[currency];
         for (const calculatingCurrency of Object.keys(baseCurrencyRates)) {
-            const currencyRate = Number((toBaseCurrencyRate * baseCurrencyRates[calculatingCurrency]).toFixed(PRECISION));
-            rates[currency][calculatingCurrency] = currencyRate;
+            rates[currency][calculatingCurrency] = Number((toBaseCurrencyRate * baseCurrencyRates[calculatingCurrency])
+                .toFixed(PRECISION));
         }
     }
+}
 
-    for (const selector of selectors) {
-        for (const currency of Object.keys(baseCurrencyRates)) {
+const addSelectorsOptions = currencies => {
+    selectors.forEach(selector => {
+        currencies.forEach(currency => {
             let option = document.createElement("option");
             option.text = currency;
             option.value = currency;
             selector.add(option);
-        }
+        })
+    })
+}
+
+const app = async () => {
+    const data = await fetchRates();
+    const {base: baseCurrency, rates: baseCurrencyRates} = data;
+    rates = {[baseCurrency]: {...baseCurrencyRates, [baseCurrency]: 1}};
+
+    calculateRates(baseCurrencyRates);
+    addSelectorsOptions(Object.keys(baseCurrencyRates).sort());
+    handle();
+}
+
+const handle = () => {
+    const userInputValue = document.getElementById('amount-input').value;
+    const parsedValue = parseFloat(userInputValue.replace(',', '.'));
+
+    if (userInputValue.length === 0) {
+        return;
     }
+    if (isNaN(parsedValue)) {
+        document.getElementById('result').innerText = 'Input value is incorrect. Please enter a correct value.';
+        return;
+    }
+
+    const [fromSelector, toSelector] = selectors;
+    const fromCurrency = fromSelector.value;
+    const toCurrency = toSelector.value;
+    const result = (parsedValue * rates[fromCurrency][toCurrency]);
+
+    const fromFormatted = new Intl.NumberFormat(LOCALE, {
+        style: 'currency',
+        currency: fromCurrency
+    }).format(parsedValue);
+    const toFormatted = new Intl.NumberFormat(LOCALE, {style: 'currency', currency: toCurrency}).format(result);
+
+    document.getElementById('result').innerText = `${fromFormatted} → ${toFormatted}`;
 }
 
 const submitHandler = e => {
     e.preventDefault();
-    const value = document.getElementById('amount-input').value;
-    const fromCurrency = selectors[0].value;
-    const toCurrency = selectors[1].value;
-    const result = (value * rates[fromCurrency][toCurrency]).toPrecision(PRECISION);
-
-    const from = new Intl.NumberFormat(LOCALE, { style: 'currency', currency: fromCurrency }).format(value);
-    const to = new Intl.NumberFormat(LOCALE, { style: 'currency', currency: toCurrency }).format(result);
-
-    document.getElementById('result').innerText = `${from} -> ${to}`;
+    handle();
 }
 
-for(const selector of selectors) {
+selectors.forEach(selector => {
     selector.addEventListener('change', e => {
         e.stopPropagation();
-        console.log('changed')
+        handle();
     })
-}
+})
+
+document.getElementById('amount-input').addEventListener('keyup', handle);
 document.getElementById('convert-form').addEventListener('submit', submitHandler);
 
-fn();
-setInterval(fn, API_CALL_INTERVAL)
+app().then(() => {
+    loader.hidden = true;
+    document.getElementById('convert-form').hidden = false;
+});
+
+setInterval(app, API_CALL_INTERVAL_MS);
